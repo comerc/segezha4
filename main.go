3a2290b5ae0176fbd6cad7490d451acc347b2942
comerc <andrew.kachanov@gmail.com>
2021-02-08 01:55:51 +0300

    Try log

--- main.go
+++ main.go
@@ -2,36 +2,37 @@ package main
 
 import (
 	"log"
-	"os"
-
-	tb "gopkg.in/tucnak/telebot.v2"
+	// "os"
+	// tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
-	var (
-		port      = os.Getenv("PORT")
-		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
-		token     = os.Getenv("TOKEN")      // you must add it to your config vars
-	)
+	log.Fatal("err1234")
 
-	webhook := &tb.Webhook{
-		Listen:   ":" + port,
-		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
-	}
+	// var (
+	// 	port      = os.Getenv("PORT")
+	// 	publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+	// 	token     = os.Getenv("TOKEN")      // you must add it to your config vars
+	// )
 
-	pref := tb.Settings{
-		Token:  token,
-		Poller: webhook,
-	}
+	// webhook := &tb.Webhook{
+	// 	Listen:   ":" + port,
+	// 	Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	// }
 
-	b, err := tb.NewBot(pref)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// pref := tb.Settings{
+	// 	Token:  token,
+	// 	Poller: webhook,
+	// }
 
-	b.Handle("/hello", func(m *tb.Message) {
-		b.Send(m.Sender, "Hi!")
-	})
+	// b, err := tb.NewBot(pref)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// b.Handle("/hello", func(m *tb.Message) {
+	// 	b.Send(m.Sender, "Hi!")
+	// })
 }
 
 // package main
