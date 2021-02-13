514a8469db96152ad6d3f4aaead6de2a1983b391
comerc <andrew.kachanov@gmail.com>
2021-02-13 23:15:12 +0300

    Fix url

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ $GOOGL #GOOGL
 3. Копирование ФА в портфель по тикету https://googlesheets.medium.com/bot-happens-telegram-bot-google-sheets-on-webhooks-e415509a6213
 
 [Telegram Bot - how to get a group chat id?](https://stackoverflow.com/questions/32423837/telegram-bot-how-to-get-a-group-chat-id)
+
+https://github.com/heroku/heroku-buildpack-google-chrome
--- article_cases.go
+++ article_cases.go
@@ -13,7 +13,8 @@ type ArticleCase struct {
 var ArticleCases = []ArticleCase{
 	{name: "finviz.com", url: "https://finviz.com/quote.ashx?t=%s"},
 	{name: "cathiesark.com", url: "https://cathiesark.com/ark-combined-holdings-of-%s", hasPreview: true},
-	{name: "stockscores.com", url: "https://www.stockscores.com/chart.asp?TickerSymbol=%s&TimeRange=100&Interval=d&Volume=1&ChartType=CandleStick&Stockscores=None&ChartWidth=860&ChartHeight=480&LogScale=1&Band=None&avgType1=EMA&movAvg1=20&avgType2=EMA&movAvg2=50&Indicator1=Momentum&Indicator2=RSI&Indicator3=MACD&Indicator4=AccDist&endDate=2021-2-13&CompareWith=&entryPrice=&stopLossPrice=&candles=redgreen",
+	{name: "stockscores.com", url: "https://stockscores.com/chart.asp?TickerSymbol=%s&TimeRange=100&Interval=d&Volume=1&ChartType=CandleStick&Stockscores=None&ChartWidth=860&ChartHeight=480&LogScale=1&Band=None&avgType1=EMA&movAvg1=20&avgType2=EMA&movAvg2=50&Indicator1=RSI&Indicator2=MACD&Indicator3=AccDist&CompareWith=&entryPrice=&stopLossPrice=&candles=redgreen",
+		// "https://www.stockscores.com/chart.asp?TickerSymbol=%s&TimeRange=100&Interval=d&Volume=1&ChartType=CandleStick&Stockscores=None&ChartWidth=860&ChartHeight=480&LogScale=1&Band=None&avgType1=EMA&movAvg1=20&avgType2=EMA&movAvg2=50&Indicator1=Momentum&Indicator2=RSI&Indicator3=MACD&Indicator4=AccDist&CompareWith=&entryPrice=&stopLossPrice=&candles=redgreen",
 		// "https://stockscores.com/chart.asp?TickerSymbol=%s&TimeRange=365&Interval=d&Volume=1&ChartType=CandleStick&Stockscores=None&ChartWidth=1920&ChartHeight=430&LogScale=1&Band=None&avgType1=EMA&movAvg1=20&avgType2=EMA&movAvg2=50&Indicator1=Momentum&Indicator2=RSI&Indicator3=MACD&Indicator4=AccDist&CompareWith=&entryPrice=&stopLossPrice=&candles=redgreen",
 		hasPreview: true},
 	{name: "shortvolume.com", url: "https://shortvolume.com/chart_engine/draw_chart.php?Symbol=%s&TimeRange=100", hasPreview: true},
