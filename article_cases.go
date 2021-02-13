45d00aa1ef31bbaab142c22e24e26a7a8bbba51d
comerc <andrew.kachanov@gmail.com>
2021-02-13 22:47:00 +0300

    Change TimeRange

--- article_cases.go
+++ article_cases.go
@@ -14,7 +14,7 @@ var ArticleCases = []ArticleCase{
 	{name: "finviz.com", url: "https://finviz.com/quote.ashx?t=%s"},
 	{name: "cathiesark.com", url: "https://cathiesark.com/ark-combined-holdings-of-%s", hasPreview: true},
 	{name: "stockscores.com", url: "https://stockscores.com/chart.asp?TickerSymbol=%s&TimeRange=365&Interval=d&Volume=1&ChartType=CandleStick&Stockscores=None&ChartWidth=1920&ChartHeight=430&LogScale=1&Band=None&avgType1=EMA&movAvg1=20&avgType2=EMA&movAvg2=50&Indicator1=Momentum&Indicator2=RSI&Indicator3=MACD&Indicator4=AccDist&CompareWith=&entryPrice=&stopLossPrice=&candles=redgreen", hasPreview: true},
-	{name: "shortvolume.com", url: "https://shortvolume.com/chart_engine/draw_chart.php?Symbol=%s&TimeRange=30", hasPreview: true},
+	{name: "shortvolume.com", url: "https://shortvolume.com/chart_engine/draw_chart.php?Symbol=%s&TimeRange=365", hasPreview: true},
 	{name: "earningswhispers.com", url: "https://earningswhispers.com/stocks/%s"},
 	{name: "marketwatch.com", url: "https://marketwatch.com/investing/stock/%s"},
 	{name: "tipranks.com", url: "https://tipranks.com/stocks/%s/forecast"},
