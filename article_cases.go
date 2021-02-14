0e0b2bcc95c2e2f095d091fa2321afeb013e9d03
comerc <andrew.kachanov@gmail.com>
2021-02-14 10:06:05 +0300

    Change struct to map

--- article_cases.go
+++ article_cases.go
@@ -1,56 +1,56 @@
 package main
 
-import (
-	"strings"
-)
+// import (
+// 	"strings"
+// )
 
 // ArticleCase struct
-type ArticleCase struct {
-	name string
-	url  string
-}
+// type ArticleCase struct {
+// 	name string
+// 	url  string
+// }
 
 // TODO: чаще используемые перемещать наверх
 // TODO: конфигурировать через интерфейс
 
-// TODO: map
+// TODO: map https://habr.com/ru/post/457728/
 // m := make(map[key_type]value_type)
 // m := new(map[key_type]value_type)
 // var m map[key_type]value_type
 // m := map[key_type]value_type{key1: val1, key2: val2}
 
-// ArticleCases slice
-var ArticleCases = []ArticleCase{
-	{name: "finviz.com", url: "https://finviz.com/quote.ashx?t=%s"},
-	{name: "cathiesark.com", url: "https://cathiesark.com/ark-combined-holdings-of-%s"},
-	{name: "marketwatch.com", url: "https://marketwatch.com/investing/stock/%s"},
-	{name: "stockscores.com",
-		url: "https://stockscores.com/chart.asp?TickerSymbol=%s&TimeRange=100&Interval=d&Volume=1&ChartType=CandleStick&Stockscores=None&ChartWidth=860&ChartHeight=480&LogScale=1&Band=None&avgType1=EMA&movAvg1=20&avgType2=EMA&movAvg2=50&Indicator1=RSI&Indicator2=MACD&Indicator3=AccDist&CompareWith=&entryPrice=&stopLossPrice=&candles=redgreen",
-		// url: "https://www.stockscores.com/chart.asp?TickerSymbol=%s&TimeRange=100&Interval=d&Volume=1&ChartType=CandleStick&Stockscores=None&ChartWidth=860&ChartHeight=480&LogScale=1&Band=None&avgType1=EMA&movAvg1=20&avgType2=EMA&movAvg2=50&Indicator1=Momentum&Indicator2=RSI&Indicator3=MACD&Indicator4=AccDist&CompareWith=&entryPrice=&stopLossPrice=&candles=redgreen",
-		// url: "https://stockscores.com/chart.asp?TickerSymbol=%s&TimeRange=365&Interval=d&Volume=1&ChartType=CandleStick&Stockscores=None&ChartWidth=1920&ChartHeight=430&LogScale=1&Band=None&avgType1=EMA&movAvg1=20&avgType2=EMA&movAvg2=50&Indicator1=Momentum&Indicator2=RSI&Indicator3=MACD&Indicator4=AccDist&CompareWith=&entryPrice=&stopLossPrice=&candles=redgreen",
-	},
-	{name: "shortvolume.com", url: "https://shortvolume.com/chart_engine/draw_chart.php?Symbol=%s&TimeRange=100"},
-	{name: "marketbeat.com", url: "https://marketbeat.com/stocks/%s"},
-	{name: "earningswhispers.com", url: "https://earningswhispers.com/stocks/%s"},
-	// {name: "tipranks.com", url: "https://tipranks.com/stocks/%s/forecast"},
-	{name: "barchart.com", url: "https://barchart.com/stocks/quotes/%s/overview"},
-	{name: "gurufocus.com", url: "https://gurufocus.com/stock/%s/summary"},
-	{name: "stockrow.com", url: "https://stockrow.com/%s"},
-	{name: "stockanalysis.com", url: "https://stockanalysis.com/stocks/%s/"},
-	{name: "finasquare.com", url: "https://www.finasquare.com/stocks/%s/company-info/overview"},
+// ArticleCases map
+var ArticleCases = make(map[string]string)
+
+// InitializeArticleCases func
+func InitializeArticleCases() {
+	ArticleCases["finviz.com"] = "https://finviz.com/quote.ashx?t=%s"
+	ArticleCases["cathiesark.com"] = "https://cathiesark.com/ark-combined-holdings-of-%s"
+	ArticleCases["marketwatch.com"] = "https://marketwatch.com/investing/stock/%s"
+	ArticleCases["stockscores.com"] =
+		"https://stockscores.com/chart.asp?TickerSymbol=%s&TimeRange=100&Interval=d&Volume=1&ChartType=CandleStick&Stockscores=None&ChartWidth=860&ChartHeight=480&LogScale=1&Band=None&avgType1=EMA&movAvg1=20&avgType2=EMA&movAvg2=50&Indicator1=RSI&Indicator2=MACD&Indicator3=AccDist&CompareWith=&entryPrice=&stopLossPrice=&candles=redgreen"
+	ArticleCases["shortvolume.com"] = "https://shortvolume.com/chart_engine/draw_chart.php?Symbol=%s&TimeRange=100"
+	ArticleCases["marketbeat.com"] = "https://marketbeat.com/stocks/%s"
+	ArticleCases["earningswhispers.com"] = "https://earningswhispers.com/stocks/%s"
+	// ArticleCases["tipranks.com"] = "https://tipranks.com/stocks/%s/forecast"
+	ArticleCases["barchart.com"] = "https://barchart.com/stocks/quotes/%s/overview"
+	ArticleCases["gurufocus.com"] = "https://gurufocus.com/stock/%s/summary"
+	ArticleCases["stockrow.com"] = "https://stockrow.com/%s"
+	ArticleCases["stockanalysis.com"] = "https://stockanalysis.com/stocks/%s/"
+	ArticleCases["finasquare.com"] = "https://www.finasquare.com/stocks/%s/company-info/overview"
 }
 
 // GetExactArticleCase function
-func GetExactArticleCase(search string) *ArticleCase {
-	var result *ArticleCase
-	if len(search) > 0 {
-		search = strings.ToUpper(search)
-		for _, articleCase := range ArticleCases {
-			if articleCase.name == search {
-				result = &articleCase
-				break
-			}
-		}
-	}
-	return result
-}
+// func GetExactArticleCase(search string) *ArticleCase {
+// 	var result *ArticleCase
+// 	if len(search) > 0 {
+// 		search = strings.ToUpper(search)
+// 		for _, articleCase := range ArticleCases {
+// 			if articleCase.name == search {
+// 				result = &articleCase
+// 				break
+// 			}
+// 		}
+// 	}
+// 	return result
+// }
