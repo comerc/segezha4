0718b317491a9ef1b3e55415998818f9f44c2230
comerc <andrew.kachanov@gmail.com>
2021-02-28 20:53:16 +0300

    Add volume

--- main.go
+++ main.go
@@ -569,8 +569,14 @@ func by(s string) string {
 // }
 
 func sendBarChart(b *tb.Bot, chatID int64, symbol string) bool {
-	linkURL := "https://www.barchart.com/stocks/quotes/%s/technical-chart%s?plot=CANDLE&volume=1&data=I:5&density=L&pricesOn=0&asPctChange=0&logscale=0&im=5&indicators=EXPMA(100);EXPMA(50);EXPMA(20);EXPMA(200);WMA(9);EXPMA(500)&sym=%[1]s&grid=1&height=625&studyheight=100"
-	screenshot := ss.MakeScreenshotForBarChart(fmt.Sprintf(linkURL, symbol, "/fullscreen"))
+	volume := func() string {
+		if symbol == "$VIX" {
+			return "0"
+		}
+		return "total"
+	}()
+	linkURL := "https://www.barchart.com/stocks/quotes/%s/technical-chart%s?plot=CANDLE&volume=%s&data=I:5&density=L&pricesOn=0&asPctChange=0&logscale=0&im=5&indicators=EXPMA(100);EXPMA(50);EXPMA(20);EXPMA(200);WMA(9);EXPMA(500)&sym=%[1]s&grid=1&height=625&studyheight=100"
+	screenshot := ss.MakeScreenshotForBarChart(fmt.Sprintf(linkURL, symbol, "/fullscreen", volume))
 	if len(screenshot) == 0 {
 		return false
 	}
