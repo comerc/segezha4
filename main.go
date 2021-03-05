ccfeb601f83c818f591870673843f9187b9ac035
comerc <andrew.kachanov@gmail.com>
2021-03-05 18:44:07 +0300

    Debug m.Sender

--- main.go
+++ main.go
@@ -99,6 +99,11 @@ func main() {
 	})
 	b.Handle(tb.OnText, func(m *tb.Message) {
 		log.Println("****")
+		if m.Sender != nil {
+			log.Println(m.Sender.Username)
+			log.Println(m.Sender.FirstName)
+			log.Println(m.Sender.LastName)
+		}
 		log.Println(m.Chat.Username)
 		log.Println(m.Text)
 		log.Println("****")
