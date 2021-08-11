801a15af458b39ccb52573c4fc09ba8f1dee6fad
comerc <andrew.kachanov@gmail.com>
2021-08-11 17:20:08 +0300

    Fix /mw

--- screenshot/marketwatch.go
+++ screenshot/marketwatch.go
@@ -46,7 +46,8 @@ func MakeScreenshotForMarketWatch(linkURL string) []byte {
 			chromedp.SetAttributeValue("body > div.container.container--body > div.region.region--intraday > div.column.column--full > div.element.element--company > div.row > div.quote-actions", "style", "display:none"),
 			chromedp.SetAttributeValue("body > div.container.container--body > div.region.region--intraday > div.column.column--primary > mw-chart.element.element--chart > label.toggle--chart", "style", "display:none"),
 			chromedp.SetAttributeValue("body > div.container.container--body > div.region.region--intraday > div.column.column--primary > mw-chart.element.element--chart > div.chart__options", "style", "display:none"),
-			// chromedp.SetAttributeValue("body > div.container.container--trending", "style", "display:none"),
+			chromedp.ActionFunc(hideIfExists("body > div.container.container--trending")),
+			chromedp.ActionFunc(hideIfExists("body > div.message-container")),
 			chromedp.Screenshot("body > div.container.container--body > div.region.region--intraday", &buf, chromedp.NodeVisible),
 		}
 	}()); err != nil {
